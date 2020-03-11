9c0fef2e372b369184ee3358d4ab700510b203ba
James Rhodes <vincerhodes@gmail.com>
2020-03-11 17:27:50 +0800

    updated with graphics

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -1,85 +1,131 @@
 
-// constants for choices
+// initialise constants
 const choices = {rock: 0, paper: 1, scissors: 2};
-const continueOptions = {y: true, yes: true, n: false, no: false};
-const resultOptions = ["You lose, better luck next time!", "You win, congratulations!", "Game is a tie!"];
+const images = {rock: "/img/rock2.png",
+  paper: "/img/paper2.png",
+  scissors: "/img/scissors2.png"
+};
+const resultOptions = ["You lose!", "You win!", "It's a tie!"];
 
-main();
+// initialise variables
+var tracker = {game_in_progress: false};
+var result;
 
-//main function to play the game
-function main() {
+// setup DOM selections
+const startButton = document.querySelector('.btn');
+const rpsButtons = document.querySelectorAll('.rps');
+const commentary = document.querySelector('.commentary');
+const playerChoiceImage = document.querySelector('#player-choice');
+const computerChoiceImage = document.querySelector('#computer-choice');
+const scores = document.querySelectorAll('.score');
 
-  // initial setup of variables for playing the game
-  var continueChoice = "y",
-    playerSelection,
-    computerSelection;
+// add event listeners
+startButton.addEventListener('click', () => initializeGame());
 
-  // use a while loop to continue playing until user chooses to stop
-  while (continueOptions[continueChoice]) {
+rpsButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (tracker.game_in_progress) {
+      playRound(button.id);
+    };
+  });
+});
 
-    var continueText;
+function initializeGame() {
+  commentary.textContent = "First to 5, game on!"
+  tracker = {game_in_progress: true,
+    rounds_played: 0,
+    player: 0,
+    computer: 0,
+    results: []
+  };
+  updateScores();
+};
 
-    // take input from the player
-    do {
-      playerSelection = prompt("Please choose rock, paper or scissors (type the full word)").toLowerCase();
-      // handle invalid selection with console output
-      if (!(playerSelection in choices)) {
-        console.log("Invalid choice, please type 'rock' or 'paper' or 'scissors'");
-      }
-    }
-    // loops back to the start of the do-while loop if selection is invalid
-    while (!(playerSelection in choices));
-
-    // get the computer selection
-    computerSelection = computerPlay();
-
-    // simulate the game given the choices and output the result to the console
-    outcome = playRound(playerSelection, computerSelection);
-
-    continueText = `Player chose ${playerSelection}, computer chose ${computerSelection}\n`;
-    continueText += resultOptions[outcome] + '\n\n';
-
-    // play again code
-    do  {
-      continueText += "Do you wish to play again? (y/n)"
-      continueChoice = prompt(continueText);
-      // handle invalid selection with console output
-      if (!(continueChoice in continueOptions)) {
-        continueText = "Invalid choice, please enter 'y' or 'n' followed by enter\n\n";
-      }
-    }
-    // loops back to the start of the do-while loop if selection is invalid
-    while (!(continueChoice in continueOptions));
-
-  }
-
+function updateScores() {
+  scores[0].textContent = tracker.player;
+  scores[1].textContent = tracker.computer;
+  if (tracker.player > tracker.computer) {
+    console.log("blah");
+    scores[0].style.color = "green";
+    scores[1].style.color = "red";
+  } else if (tracker.rounds_played == 0) {
+    scores[0].style.color = "lightgrey";
+    scores[1].style.color = "lightgrey";
+  } else if (tracker.player == tracker.computer) {
+    scores[0].style.color = "orange";
+    scores[1].style.color = "orange";
+  } else {
+    scores[1].style.color = "green";
+    scores[0].style.color = "red";
+  };
 }
 
-// code to play the game
-function playRound(playerSelection, computerSelection) {
-  // calculate mathematical result for 0 = lose, 1 = win and 2 = draw (1st line calculates win/lose, 2nd line adds draws in)??
+// select a choice randomly (pseudo) from the options available
+function computerPlay() {
+  // var delay = 100;
+  // for (var i=0; i<9; i++) {
+  //   choice =  Object.keys(choices)[Math.floor(Math.random() * 3)];
+  //   computerChoiceImage.src = images[choice];
+  //   sleep(delay);
+  //   if(i>5) {
+  //     delay=300
+  //   } else if(i>2) {
+  //     delay=200
+  //   };
+  // };
+  choice =  Object.keys(choices)[Math.floor(Math.random() * 3)];
+  computerChoiceImage.src = images[choice];
+  return choice;
+}
 
-  // calulate result
-  if (choices[playerSelection] == choices[computerSelection]) {
+// play a round
+function playRound(playerChoice) {
+  playerChoiceImage.src = images[playerChoice];
+  computerChoice = computerPlay();
+  // calculate result
+  if (choices[playerChoice] == choices[computerChoice]) {
     result = 2;
   } else {
-    switch (playerSelection) {
+    switch (playerChoice) {
       case "rock":
-        result = +(computerSelection == "scissors");
+        result = +(computerChoice == "scissors");
         break;
       case "paper":
-        result = +(computerSelection == "rock");
+        result = +(computerChoice == "rock");
         break;
       case "scissors":
-        result = +(computerSelection == "paper");
+        result = +(computerChoice == "paper");
         break;
       };
   };
 
-  return result;
-}
+  switch (result) {
+    case 0:
+      commentary.textContent = `${playerChoice} loses to ${computerChoice}`;
+      result = "lose";
+      tracker.computer++;
+      break;
+    case 1:
+      commentary.textContent = `${playerChoice} beats ${computerChoice}`;
+      result = "win";
+      tracker.player++;
+      break;
+    case 2:
+      commentary.textContent = `${playerChoice} draws against ${computerChoice}`;
+      result = "draw";
+      break;
+  };
 
-// code to generate computer's turn
-function computerPlay() {
-  return Object.keys(choices)[Math.floor(Math.random() * 3)];
-}
+  tracker.results.push(result);
+  tracker.rounds_played++;
+  updateScores();
+  console.log(tracker);
+};
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+};
